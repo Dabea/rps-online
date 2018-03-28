f0fc3476f5cf1f4e0106d3f0784a74a7e44f839f
Abraham Anderson <abeis@msn.com>
2018-03-28 17:40:31 -0500

    setup firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,35 @@
-alert('tset');
+
+var config = {
+  apiKey: "AIzaSyBHCj1KWI0TqfaNlffSO5hh5quqsGI4r8E",
+  authDomain: "rps-online-ver.firebaseapp.com",
+  databaseURL: "https://rps-online-ver.firebaseio.com",
+  projectId: "rps-online-ver",
+  storageBucket: "rps-online-ver.appspot.com",
+  messagingSenderId: "738473785317"
+};
+
+
+firebase.initializeApp(config);
+
+const database = firebase.database().ref();
+
+database.set({
+    "messages": {
+        "1": {
+                "name": "eclarke",
+                "message": "The relay seems to be malfunctioning.",
+                "timestamp": 1459361875337
+            },
+        "2": {
+            "name": "Susy",
+            "message": "Looks good to me.",
+            "timestamp": 1459361877337
+        },    
+    }        
+});
+
+database.orderByValue().on("value", function(data) {
+    console.log(data.val().messages[1].message);
+   const messageList = data.val().messages;
+   messageList.forEach(message => $('.chat-area').append(`<div>${message.name} :  ${message.message}</div`) )
+ });
