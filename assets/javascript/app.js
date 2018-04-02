2debe4424117f4e6c511e4ed420b3943086bf833
Abraham Anderson <abeis@msn.com>
2018-04-02 16:49:41 -0500

    added functionality to RPS game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,10 @@ const name = prompt('What is your name?');
 
 firebase.initializeApp(config);
 
-const database = firebase.database().ref().child('messages');
+const database = firebase.database().ref('messages');
+const db = firebase.database();
+
+const playerDb = firebase.database();
 
 
 // database.set({
@@ -30,27 +33,21 @@ const database = firebase.database().ref().child('messages');
 //     }        
 // });
 
-// database.on("value", function(data) {
-    
+
+
+db.ref('messages').once('value').then(function(data) {
+  
    
-//   data.forEach(function(childSnapshot) {
-//     console.log(childSnapshot)
-//     var messageData = childSnapshot.val();
-//     console.log(messageData);
-//     $('.chat-area').append(`<div>${messageData.name}:  ${messageData.message}</div`);
+  database.onDisconnect().set(name + " has disconnected~");
+  const message = `${name} has disconnected`;
+
     
-// });
-// });
+});
+
 
 
  $('.chat-submit').on('click', function(){
-    const message = $('.chat-input').val();
-    $('.chat-input').val('');
-     database.push().set({
-        "name": name,
-        "message": message,
-        "timestamp": 1459361875337
-     })
+  submitMessage();
  })
 
 
@@ -62,21 +59,113 @@ const database = firebase.database().ref().child('messages');
 
 $('.join').on('click', function(){
 
-  database.set({
-    "player1":{
-      "active": name,
-      "choice": null,
-      "wins": 0,
+  console.log('fire')
+  playerDb.ref('player').once('value', function(snapshot){
+    console.log(!snapshot.exists())
+    if(!snapshot.exists()) {
+      playerDb.ref('player/player1').onDisconnect().remove()
+      playerDb.ref("player").update({
+        "player1":{
+          "name": name,
+          "choice": null,
+          "wins": 0,
+        }
+      })
+      $('.player1').addClass('active')
+    console.log('player1 set')
     }
-  })
-  $('.player1').addClass('active')
+    else if (snapshot.val().player1 && !snapshot.val().player2) {
+      playerDb.ref('player/player2').onDisconnect().remove()
+      playerDb.ref("player").update({
+        "player2":{
+          "name": name,
+          "choice": null,
+          "wins": 0,
+        }
+      })
+      $('.player2').addClass('active')
+      console.log('player2 set')
+    }else{
+      alert('sorry the game is full');
+    }
+    })
+
+
+   
+
+    console.log('end')
+
+
+  
 })
 
-database.orderByChild('name').equalTo('abraham').on("value", function(snapshot) {
-  console.log(snapshot.val());
-  snapshot.forEach(function(data) {
-console.log(data.val().remove());
-  });
-});
+// database.orderByChild('name').equalTo('abraham').on("value", function(snapshot) {
+//   console.log(snapshot.val());
+//   snapshot.forEach(function(data) {
+// console.log(data.val().remove());
+//   });
+// });
+
+/**
+ * Will input a message in to the database
+ * 
+ * @param {*} message 
+ */
+function submitMessage(){
+    const message = $('.chat-input').val();
+    $('.chat-input').val('');
+     database.push().set({
+        "name": name,
+        "message": message,
+        "timestamp": 1459361875337
+     })
+}
+
+
+
+
+
+
+
+/**************** Show Active Players   ***********/
+playerDb.ref('player').on('value', function(snapshot){
+      if(snapshot.val().player1){
+        $('.player1').addClass('active')
+        $('.player1-name').text(snapshot.val().player1.name)
+      }else{
+        $('.player1').removeClass('active')
+        $('.player1-name').text('Waitting for Player1')
+      }
+
+      if(snapshot.val().player2){
+        $('.player2').addClass('active')
+        $('.player2-name').text(snapshot.val().player2.name)
+      }else{
+        $('.player2').removeClass('active')
+        $('.player2-name').text('Waitting for Player2')
+      }
+
+      if(snapshot.val().player1 && snapshot.val().player2){
+        
+          $('.choice-list').empty();
+          $('.choice-list').append('<li data-choice="rock" class="choices">rock</li> <li data-choice="papper" class="choices">papper</li><li data-choice="Sisciors" class="choices">Sisciors</li>');
+          enableRPSChoice();
+    
+
+    
+      
+      }
+})
+
+
+
+function enableRPSChoice(){
+  $('.choices').on("click", function(event){
+    const choice = $(event.target).data('choice')
+    console.log(choice);
+    $('.choices').off()
+    
+})
+}
 
 
