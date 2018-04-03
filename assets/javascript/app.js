d42c806a99b0e4edb7aca3322a56b2163355cd04
Abraham <abeis@msn.com>
2018-04-02 22:23:47 -0500

    removed extra code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,196 +8,143 @@ var config = {
   messagingSenderId: "738473785317"
 };
 
-const name = prompt('What is your name?');
-
 firebase.initializeApp(config);
 
+const name = prompt('What is your name?');
 const database = firebase.database().ref('messages');
 const db = firebase.database();
-
 const playerDb = firebase.database();
 
-
-// database.set({
-//     "messages": {
-//         "1": {
-//                 "name": "eclarke",
-//                 "message": "The relay seems to be malfunctioning.",
-//                 "timestamp": 1459361875337
-//             },
-//         "2": {
-//             "name": "Susy",
-//             "message": "Looks good to me.",
-//             "timestamp": 1459361877337
-//         },    
-//     }        
-// });
-
-
-
-db.ref('messages').once('value').then(function(data) {
-  
-   
+// set disconnect message
+db.ref('messages').once('value').then(function (data) {
   database.onDisconnect().set(name + " has disconnected~");
   const message = `${name} has disconnected`;
-
-    
 });
 
-
-
- $('.chat-submit').on('click', function(){
+//submits the chat message ****************** Event
+$('.chat-submit').on('click', function () {
   submitMessage();
- })
+})
 
 
- database.on('child_added', function(message) {
+database.on('child_added', function (message) {
   var message = message.val();
   $('.chat-area').append(`<div>${message.name}:  ${message.message}</div`);
 });
 
 
-$('.join').on('click', function(){
-  playerDb.ref('player').once('value', function(snapshot){
-    if(!snapshot.exists() || !snapshot.val().player1 ) {
+$('.join').on('click', function () {
+  playerDb.ref('player').once('value', function (snapshot) {
+    if (!snapshot.exists() || !snapshot.val().player1) {
       playerDb.ref('player/player1').onDisconnect().remove()
       playerDb.ref("player").update({
-        "player1":{
+        "player1": {
           "name": name,
           "choice": null,
           "wins": 0,
         }
       })
       $('.player1').addClass('active')
-    console.log('player1 set')
     }
     else if (snapshot.val().player1 && !snapshot.val().player2) {
       playerDb.ref('player/player2').onDisconnect().remove()
       playerDb.ref("player").update({
-        "player2":{
+        "player2": {
           "name": name,
           "choice": null,
           "wins": 0,
         }
       })
       $('.player2').addClass('active')
-      console.log('player2 set')
-    }else{
+    } else {
       alert('sorry the game is full');
     }
-    })
-
-
-   
-
-    console.log('end')
-
-
-  
+  })
 })
-
-// database.orderByChild('name').equalTo('abraham').on("value", function(snapshot) {
-//   console.log(snapshot.val());
-//   snapshot.forEach(function(data) {
-// console.log(data.val().remove());
-//   });
-// });
 
 /**
  * Will input a message in to the database
  * 
  * @param {*} message 
  */
-function submitMessage(){
-    const message = $('.chat-input').val();
-    $('.chat-input').val('');
-     database.push().set({
-        "name": name,
-        "message": message,
-        "timestamp": 1459361875337
-     })
+function submitMessage() {
+  const message = $('.chat-input').val();
+  $('.chat-input').val('');
+  database.push().set({
+    "name": name,
+    "message": message,
+    "timestamp": 1459361875337
+  })
 }
-
-
-
-
-
-
 
 /**************** Show Active Players   ***********/
-playerDb.ref('player').on('value', function(snapshot){
-      if(snapshot.val().player1){
-        $('.player1').addClass('active')
-        $('.player1-name').text(snapshot.val().player1.name)
-      }else{
-        $('.player1').removeClass('active')
-        $('.player1-name').text('Waitting for Player1')
-      }
+playerDb.ref('player').on('value', function (snapshot) {
+  if (snapshot.val().player1) {
+    $('.player1').addClass('active')
+    $('.player1-name').text(snapshot.val().player1.name)
+  } else {
+    $('.player1').removeClass('active')
+    $('.player1-name').text('Waitting for Player1')
+  }
 
-      if(snapshot.val().player2){
-        $('.player2').addClass('active')
-        $('.player2-name').text(snapshot.val().player2.name)
-      }else{
-        $('.player2').removeClass('active')
-        $('.player2-name').text('Waitting for Player2')
-      }
+  if (snapshot.val().player2) {
+    $('.player2').addClass('active')
+    $('.player2-name').text(snapshot.val().player2.name)
+  } else {
+    $('.player2').removeClass('active')
+    $('.player2-name').text('Waitting for Player2')
+  }
 
-      if(snapshot.val().player1 && snapshot.val().player2){
-          $('.choice-list').empty();
-          $('.choice-list').append('<li data-choice="rock" class="choices">rock</li> <li data-choice="papper" class="choices">papper</li><li data-choice="Sisciors" class="choices">Sisciors</li>');
-          enableRPSChoice();
-      }
+  if (snapshot.val().player1 && snapshot.val().player2) {
+    $('.choice-list').empty();
+    $('.choice-list').append('<li data-choice="rock" class="choices">rock</li> <li data-choice="papper" class="choices">papper</li><li data-choice="Sisciors" class="choices">Sisciors</li>');
+    enableRPSChoice();
+  }
 
-      if((snapshot.val().player1.choice != null) && (snapshot.val().player2.choice != null) ){
+  if ((snapshot.val().player1.choice != null) && (snapshot.val().player2.choice != null)) {
 
-        if(snapshot.val().player1.choice === snapshot.val().player2.choice){
-          alert('TIE');
-          return;
-        }
-        
-        if(snapshot.val().player1.choice === "rock"){
-          if(snapshot.val().player2.choice == "Sisciors"){ alert(`player ${snapshot.val().player1.name} wins`)}
-          if(snapshot.val().player2.choice == "papper"){ alert(`player ${snapshot.val().player2.name} wins`)}
-        }
-      
-        if(snapshot.val().player1.choice === "papper"){
-          if(snapshot.val().player2.choice == "rock"){ alert(`player ${snapshot.val().player1.name} wins`)}
-          if(snapshot.val().player2.choice == "Sisciors"){ alert(`player ${snapshot.val().player2.name} wins`)}
-        }
-      
-        if(snapshot.val().player1.choice === "Sisciors"){
-          if(snapshot.val().player2.choice == "papper"){ alert(`player ${snapshot.val().player1.name} wins`)}
-          if(snapshot.val().player2.choice == "rock"){ alert(`player ${snapshot.val().player2.name} wins`)}
-        }
-      
-      
-      }
-      console.log('fire');
+    if (snapshot.val().player1.choice === snapshot.val().player2.choice) {
+      alert('TIE');
+      return;
+    }
+
+    if (snapshot.val().player1.choice === "rock") {
+      if (snapshot.val().player2.choice == "Sisciors") { alert(`player ${snapshot.val().player1.name} wins`) }
+      if (snapshot.val().player2.choice == "papper") { alert(`player ${snapshot.val().player2.name} wins`) }
+    }
+
+    if (snapshot.val().player1.choice === "papper") {
+      if (snapshot.val().player2.choice == "rock") { alert(`player ${snapshot.val().player1.name} wins`) }
+      if (snapshot.val().player2.choice == "Sisciors") { alert(`player ${snapshot.val().player2.name} wins`) }
+    }
+
+    if (snapshot.val().player1.choice === "Sisciors") {
+      if (snapshot.val().player2.choice == "papper") { alert(`player ${snapshot.val().player1.name} wins`) }
+      if (snapshot.val().player2.choice == "rock") { alert(`player ${snapshot.val().player2.name} wins`) }
+    }
+
+
+  }
+  console.log('fire');
 })
 
 
 
-function enableRPSChoice(){
-  $('.choices').on("click", function(event){
+function enableRPSChoice() {
+  $('.choices').on("click", function (event) {
     const choice = $(event.target).data('choice')
     $('.choices').off()
-    playerDb.ref('player').once('value', function(snapshot){
-        if(snapshot.val().player1.name === name){
-          $('.player1-choice').text(choice)
-          playerDb.ref('player/player1').update({'choice':choice})
-          
-        }
-        if(snapshot.val().player2.name === name){
-          $('.player2-choice').text(choice)
-          playerDb.ref('player/player2').update({'choice':choice})
-        }
+    playerDb.ref('player').once('value', function (snapshot) {
+      if (snapshot.val().player1.name === name) {
+        $('.player1-choice').text(choice);
+        playerDb.ref('player/player1').update({ 'choice': choice });
+
+      }
+      if (snapshot.val().player2.name === name) {
+        $('.player2-choice').text(choice);
+        playerDb.ref('player/player2').update({ 'choice': choice });
+      }
     });
-    
-})
-}
 
-
-function chooseWinner(player1, player2){
- 
- 
+  })
 }
